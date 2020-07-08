cbb3336600775dcae728c985f6c183cd0dd301f7
Anton Georgesco <antongeorgescu@gmail.com>
2020-07-08 15:54:13 -0400

    Added comment line re #missing values per digit

--- MNIST_Digit_Image_Tester.py
+++ MNIST_Digit_Image_Tester.py
@@ -24,8 +24,8 @@ os.makedirs(f"./datasets/{RUN_FOLDER}")
 os.makedirs(f"./converted/{RUN_FOLDER}")
 
 # =======================================================================
-MAX_MISSED_VALUES_PER_TEST_BATCH = 3000000  # out of 7,840,000 pixels
-MIN_MISSED_VALUES_PER_DIGIT = 400
+MAX_MISSED_VALUES_PER_TEST_BATCH = 4000000  # out of 7,840,000 pixels
+MIN_MISSED_VALUES_PER_DIGIT = 425
 SELECTED_DIGITS = 10
 # =======================================================================
 
@@ -82,6 +82,7 @@ if (len(flat_list_doped_images) == 0):
     sys.exit(f"No samples found with #missed values per digit grater than {MIN_MISSED_VALUES_PER_DIGIT}.")
 list_doped_images_selected = np.random.choice(flat_list_doped_images,SELECTED_DIGITS,replace=False)
 print(list_doped_images_selected)
+fExecutionSummary.write(f"Selected randonly 10 digits with #missing values grater than {MIN_MISSED_VALUES_PER_DIGIT}\r\n"}
 fExecutionSummary.write(f"List of selected digits: {list_doped_images_selected}\r\n")
 
 # save the selected SELECTED_DIGITS digit original images in .png files
